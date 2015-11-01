2a1243b8902f0c45ceddff453bf29490b9c680a4
Dustin Williams <dwilliams@manifestcorp.com>
2015-10-31 21:30:29 -0400

    added a new auto run main ckass

--- src/main/java/katas/dustin/GameOfLifeAutoRun.java
+++ src/main/java/katas/dustin/GameOfLifeAutoRun.java
@@ -1,10 +1,40 @@
 package katas.dustin;
 
+import java.util.Scanner;
+
+import katas.dustin.gameoflife.GameOfLife;
+
 public class GameOfLifeAutoRun {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("This application will print out a 'living' game board.");
+		System.out.println("Please enter the number of generations you wish to run.");
 
+		Scanner sc = new Scanner(System.in);
+	    String line = sc.nextLine();
+	    sc.close();
+	    
+	    int generations = Integer.valueOf(line);
+	    GameOfLife game = new GameOfLife();
+	    char[][] newState = new char[][] {
+				{'.', '.', 'A', '.', 'A', 'A', '.', 'A'},
+				{'A', '.', 'A', '.', 'A', 'A', '.', 'A'},
+				{'.', 'A', '.', '.', 'A', 'A', 'A', 'A'},
+				{'.', '.', 'A', '.', 'A', 'A', '.', 'A'},
+				{'A', '.', 'A', '.', 'A', '.', 'A', '.'},
+				{'A', 'A', '.', 'A', '.', 'A', '.', '.'}
+		};
+		game.setState(newState);
+	    
+	    game.printBoard(System.out);
+	    for(int i = 0; i < generations; i++) {
+	    	Thread.sleep(300);
+	    	game.processSingleLifeCycle();
+	    	System.out.println();
+	    	game.printBoard(System.out);
+	    }
+	    System.out.println();
+	    
 	}
 
 }
